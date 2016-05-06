448354831a8327dc559adff9ae68aaf70fea98a1
Tighearnan Carroll <carroll-t1@email.ulster.ac.uk>
2016-05-06 12:40:01 +0100

    Added two different viewtypes to RecyclerView

--- app/src/main/java/uni/tighearnan/routepicker/PurchasedTickets/PurchasedTicketsAdapter.java
+++ app/src/main/java/uni/tighearnan/routepicker/PurchasedTickets/PurchasedTicketsAdapter.java
@@ -1,6 +1,7 @@
 package uni.tighearnan.routepicker.PurchasedTickets;
 
 import android.support.v7.widget.AppCompatCheckBox;
+import android.support.v7.widget.AppCompatImageView;
 import android.support.v7.widget.AppCompatTextView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -16,13 +17,27 @@ import uni.tighearnan.routepicker.Ticket.Ticket;
 /**
  * Created by tighearnan on 28/04/16.
  */
-public class PurchasedTicketsAdapter extends RecyclerView.Adapter<PurchasedTicketsAdapter.PurchasedTicketViewHolder> {
+public class PurchasedTicketsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
-    private ArrayList<Ticket> mTickets;
+    private static final int HEADER = 0;
+    private static final int TICKET = 1;
+
+    private ArrayList<Object> mData;
     private AdapterItemSelectedListener mItemSelectedListener;
+    private int mNonTicketTotal;
 
     public PurchasedTicketsAdapter(ArrayList<Ticket> tickets) {
-        mTickets = tickets;
+        mData = new ArrayList<>();
+//        mData.add(R.drawable.golden_gate_bridge);
+        mData.add(R.drawable.bus);
+        mData.addAll(tickets);
+
+        mNonTicketTotal = 0;
+        for(Object o : mData) {
+            if(!(o instanceof Ticket)) {
+                mNonTicketTotal++;
+            }
+        }
     }
 
     public void setItemSelectedListener(AdapterItemSelectedListener itemSelectedListener) {
@@ -30,17 +45,44 @@ public class PurchasedTicketsAdapter extends RecyclerView.Adapter<PurchasedTicke
     }
 
     @Override
-    public PurchasedTicketViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.item_purchased_ticket, parent, false);
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
-        return new PurchasedTicketViewHolder(v);
+        RecyclerView.ViewHolder vh = null;
+        switch (viewType) {
+            case HEADER:
+                View v = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.item_purchased_ticket_header, parent, false);
+                vh = new HeaderViewHolder(v);
+                break;
+            case TICKET:
+                v = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.item_purchased_ticket, parent, false);
+
+                vh = new PurchasedTicketViewHolder(v);
+                break;
+        }
+
+        return vh;
     }
 
     @Override
-    public void onBindViewHolder(PurchasedTicketViewHolder holder, int position) {
-        Ticket ticket = mTickets.get(position);
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        switch (holder.getItemViewType()) {
+            case HEADER:
+                configureHeader((HeaderViewHolder) holder, position);
+                break;
+            case TICKET:
+                configureTicket((PurchasedTicketViewHolder) holder, position);
+                break;
+        }
+    }
 
+    private void configureHeader(HeaderViewHolder holder, int position) {
+        holder.mHeader.setImageResource((Integer) mData.get(position));
+    }
+
+    private void configureTicket(PurchasedTicketViewHolder holder, int position) {
+        Ticket ticket = (Ticket) mData.get(position);
         holder.mFromTextView.setText("From: " + ticket.getFrom());
         holder.mToTextView.setText("To: " + ticket.getTo());
         holder.mUsedCheckBox.setChecked(ticket.isUsed());
@@ -48,7 +90,18 @@ public class PurchasedTicketsAdapter extends RecyclerView.Adapter<PurchasedTicke
 
     @Override
     public int getItemCount() {
-        return mTickets.size();
+        return mData.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if(mData.get(position) instanceof Ticket) {
+            return TICKET;
+        } else if(mData.get(position) instanceof Integer) {
+            return HEADER;
+        }
+
+        return -1;
     }
 
     public class PurchasedTicketViewHolder extends RecyclerView.ViewHolder {
@@ -67,9 +120,22 @@ public class PurchasedTicketsAdapter extends RecyclerView.Adapter<PurchasedTicke
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    mItemSelectedListener.onAdapterItemSelected(getAdapterPosition());
+                    mItemSelectedListener.onAdapterItemSelected(getAdapterPosition() - mNonTicketTotal);
                 }
             });
         }
+    }
+
+    public class HeaderViewHolder extends RecyclerView.ViewHolder {
+
+        AppCompatImageView mHeader;
+
+        public HeaderViewHolder(View itemView) {
+            super(itemView);
+
+            mHeader = (AppCompatImageView) itemView.findViewById(R.id.image_view_header);
+        }
+
+
     }
 }
